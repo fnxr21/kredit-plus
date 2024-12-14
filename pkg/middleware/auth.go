bb7d3805e9324113714a5faef9ee8dffc71ce000
fandi.nxr <fandinur214@gmail.com>
2024-12-15 03:00:07 +0700

    fitur:auth:jwt done||need update for image use path as best pratice

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -3,6 +3,8 @@ package middleware
 import (
 	// "log"
 
+	dto "kredit-plus/internal/dto/result"
+	jwtToken "kredit-plus/pkg/jwt"
 	"net/http"
 	"strings"
 
@@ -21,47 +23,27 @@ func Auth(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		token := c.Request().Header.Get("Authorization")
 
-		// if token == "" {
-		// 	return c.JSON(http.StatusUnauthorized, dto.ErrorResult{Status: http.StatusBadRequest, Message: "unauthorized-token"})
-		// }
+		if token == "" {
+			return c.JSON(http.StatusUnauthorized, dto.ErrorResult{Status: http.StatusBadRequest, Message: "unauthorized-token"})
+		}
 
 		token = strings.Split(token, " ")[1]
-		// claims, err := jwtToken.DecodeToken(token)
+		claims, err := jwtToken.DecodeToken(token)
 
-		// if err != nil {
-		// 	return c.JSON(http.StatusUnauthorized, dto.ErrorResult{Status: http.StatusUnauthorized, Message: "unauthorized"})
-		// }
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, dto.ErrorResult{Status: http.StatusUnauthorized, Message: "unauthorized"})
+		}
 
 		// level, ok := claims["level"].(float64)
 		// if !ok {
 		// 	return c.JSON(http.StatusUnauthorized, "unauthorized")
 		// }
 		// if level == 1 {
-		// 	status, _ := claims["status"].(string)
-		// 	if status == "manager" {
-		// 		// log.Println( "manager====")
-		// 		c.Set("managerLogin", claims)
-		// 		return next(c)
-		// 	}
-		// }
-
-		// if level == 1 || level == 2 || level == 3 {
-		// 	log.Println("user======")
-
-		// 	c.Set("userLogin", claims)
-
-		// 	return next(c)
-		// }
-
-		// if level == 22421 {
-		// 	// log.Println("admin=======")
-		// 	c.Set("adminLogin", claims)
-		// 	return next(c)
-		// }
-		// if level == 220121 {
-		// 	// log.Println("admin=======")
-		// 	c.Set("adminDBLogin", claims)
-		// 	return next(c)
+		status, _ := claims["status"].(string)
+		if status == "patner" {
+			c.Set("patnerLogin", claims)
+			return next(c)
+		}
 		// }
 
 		return c.JSON(http.StatusUnauthorized, "unauthorized-not-found")
