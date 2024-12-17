02d32a8cfd292da39bfb4807ee9d3b1b0f8b028d
fandi.nxr <fandinur214@gmail.com>
2024-12-17 13:43:21 +0700

    add:partnet-asset repository done

---
+++ internal/repository/partner-asset.go
@@ -0,0 +1,38 @@
+package repositories
+
+import (
+	"kredit-plus/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type Asset interface {
+	CreateAsset(user models.Asset) (models.Asset, error)
+	ListAsset() ([]models.Asset, error)
+	AssetByID(id int) (models.Asset, error)
+}
+
+func RepositoryAsset(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) CreateAsset(user models.Asset) (models.Asset, error) {
+	err := r.db.Create(&user).Scan(&user).
+		Error
+
+	return user, err
+}
+func (r *repository) ListAsset() ([]models.Asset, error) {
+	var asset []models.Asset
+	err := r.db.Find(&asset).
+		Error
+
+	return asset, err
+}
+func (r *repository) AssetByID(id int) (models.Asset, error) {
+	var asset models.Asset
+	err := r.db.First(&asset).
+		Error
+
+	return asset, err
+}
